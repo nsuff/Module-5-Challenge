c1262b1c7e066dc6290a25392b567d781be74754
Nino Suffoletta <nbsuffoletta@gmail.com>
2022-06-23 01:26:54 -0400

    changed stuff

--- time.js
+++ time.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
         $(".time-block").each(function () {
             var schedhour = parseInt($(this).attr("id"));
         
-            console.log(schedhour);
-            console.log(currenthour);
+            //console.log(schedhour);
+            //console.log(currenthour);
     
             if (schedhour < currenthour) {
                 $(this).addClass("past");
@@ -26,21 +26,40 @@ $(document).ready(function(){
     }
     settime();
 
+    $(".saveBtn").on("click", function (){
 
+        var textareahour = $(this).siblings(".description").attr("id");
+        var textareatext = $(this).siblings(".description").val();
+        
+        
+        console.log(textareatext);
+        console.log(textareahour);
+
+
+
+        localStorage.setItem(textareahour,textareatext);
+
+
+
+
+
+
+    })
 
     
 
-
-
-
-
-
-
-
-
-
-
-
+    $("#7 .description").val(localStorage.getItem("text7"));
+    $("#8 .description").val(localStorage.getItem("text8"));
+    $("#9 .description").val(localStorage.getItem("text9"));
+    $("#10 .description").val(localStorage.getItem("text10"));
+    $("#11 .description").val(localStorage.getItem("text11"));
+    $("#12 .description").val(localStorage.getItem("text12"));
+    $("#13 .description").val(localStorage.getItem("text13"));
+    $("#14 .description").val(localStorage.getItem("text14"));
+    $("#15 .description").val(localStorage.getItem("text15"));
+    $("#16 .description").val(localStorage.getItem("text16"));
+    $("#17 .description").val(localStorage.getItem("text17"));
+  
 
 
 
